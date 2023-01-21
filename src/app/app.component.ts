7bbc1ce5af7736c50b7326694b5c8d947e1016d3
davalbra <davalbra@espol.edu.ec>
2023-01-21 17:41:04 -0500

    init variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { INavData } from '@coreui/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'businessdash';
+  navItems: INavData[] = [
+    {
+      name: 'Dashboard',
+    },
+  ];
+  constructor() {
+    interface INavAttributes {
+      [propName: string]: any;
+    }
+
+    interface INavWrapper {
+      attributes: INavAttributes;
+      element: string;
+    }
+
+    interface INavBadge {
+      text: string;
+      color: string;
+      size?: string;
+      class?: string;
+    }
+
+    interface INavLabel {
+      class?: string;
+      variant: string;
+    }
+
+    interface INavLinkProps {
+      queryParams?: { [k: string]: any };
+      fragment?: string;
+      queryParamsHandling?: 'merge' | 'preserve' | '';
+      preserveFragment?: boolean;
+      skipLocationChange?: boolean;
+      replaceUrl?: boolean;
+      state?: { [k: string]: any };
+      routerLinkActiveOptions?: { exact: boolean };
+      routerLinkActive?: string | string[];
+    }
+
+    interface INavData {
+      name?: string;
+      url?: string | any[];
+      href?: string;
+      icon?: string;
+      iconComponent?: any;
+      badge?: INavBadge;
+      title?: boolean;
+      children?: INavData[];
+      variant?: string;
+      attributes?: INavAttributes;
+      divider?: boolean;
+      class?: string;
+      label?: INavLabel;
+      wrapper?: INavWrapper;
+      linkProps?: INavLinkProps;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarModule } from '@coreui/angular';
+import { IconModule, IconSetService } from '@coreui/icons-angular';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    IconModule,
+    SidebarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [IconSetService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
